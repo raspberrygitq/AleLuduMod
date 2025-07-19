69b76cd8d429aa01aa1526555ee6fbbc6807a479
gitq <90698953+raspberrygitq@users.noreply.github.com>
2025-07-19 22:30:33 +0200

    v1.1.0 Source Code

--- src/AleLuduMod/AleLuduModPlugin.cs
+++ src/AleLuduMod/AleLuduModPlugin.cs
@@ -4,10 +4,7 @@ using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
 using BepInEx.Configuration;
-using Reactor.Networking;
-using Reactor.Networking.Attributes;
 using System.Linq;
-using BepInEx.Logging;
 
 namespace AleLuduMod;
 
@@ -18,16 +15,16 @@ namespace AleLuduMod;
 [BepInDependency("com.slushiegoose.townofus", BepInDependency.DependencyFlags.SoftDependency)] // load after town of us
 public partial class AleLuduModPlugin : BasePlugin
 {
-    public const int MaxPlayers = 35;
-    public const int MaxImpostors = 35 / 2;
+    public const int MaxPlayers = 36;
+    public const int MaxImpostors = 36 / 2;
     public static ConfigEntry<bool> Force4Columns { get; set; }
     private Harmony Harmony { get; } = new(Id);
 
     public override void Load()
     { 
-        NormalGameOptionsV09.RecommendedImpostors = NormalGameOptionsV09.MaxImpostors = Enumerable.Repeat(35, 35).ToArray();
-        NormalGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 35).ToArray();
-        HideNSeekGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 35).ToArray();
+        NormalGameOptionsV09.RecommendedImpostors = NormalGameOptionsV09.MaxImpostors = Enumerable.Repeat(36, 36).ToArray();
+        NormalGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 36).ToArray();
+        HideNSeekGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 36).ToArray();
 
         Force4Columns = Config.Bind("Settings", "Force 4 columns", true, "Always display 4 columns in meeting, vitals, etc.");
 
---
+++ src/AleLuduMod/Patches/ChatCommands.cs
@@ -0,0 +1,120 @@
+﻿using AmongUs.GameOptions;
+using HarmonyLib;
+
+namespace AleLuduMod.Patches
+{
+    public class ChatCommands
+    {
+        public static bool system = false;
+        public static bool error = false;
+        public static bool noaccess = false;
+
+        [HarmonyPatch(typeof(ChatController), nameof(ChatController.AddChat))]
+        public class Commands
+        {
+            public static bool Prefix(ChatController __instance, [HarmonyArgument(0)] PlayerControl sourcePlayer , ref string chatText)
+            {
+                if (__instance != HudManager.Instance.Chat)
+                    return true;
+
+                // After entering the command, when you try to join the lobby it will show "X/15". Only after the game is over will there be a larger lobby.
+                // Other mods that have access to chat commands like "Town Of Us" will always display “Invalid Command.”
+                if (chatText.StartsWith("/limit "))
+                {
+                    if (GameData.Instance.GetHost() == sourcePlayer.Data)
+                    {
+                        string[] args = chatText.Split(' ');
+                        if (args.Length > 1 && int.TryParse(args[1], out int newLimit))
+                        {
+                            if (newLimit >= 4 && newLimit <= 35)
+                            {
+                                try
+                                {
+                                    GameOptionsManager.Instance.CurrentGameOptions.SetInt(Int32OptionNames.MaxPlayers, newLimit);
+                                    if (sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                                    {
+                                        system = true;
+                                        HudManager.Instance.Chat.AddChat(PlayerControl.LocalPlayer, $"A player limit has been set for: <color=#D91919FF>{newLimit}</color>");
+                                    }
+                                    return false;
+                                }
+                                catch (System.Exception)
+                                {
+                                    return false;
+                                }
+                            }
+                            else
+                            {
+                                if (sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                                {
+                                    error = true;
+                                    HudManager.Instance.Chat.AddChat(PlayerControl.LocalPlayer, "The limit must be between 4 and 35.");
+                                }
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            if (sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                            {
+                                error = true;
+                                HudManager.Instance.Chat.AddChat(PlayerControl.LocalPlayer, "Use /limit [number]. Example: /limit 20");
+                            }
+                            return false;
+                        }
+                    }
+                    else if (sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                    {
+                        if (sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId)
+                        {
+                            noaccess = true;
+                            HudManager.Instance.Chat.AddChat(PlayerControl.LocalPlayer, "You don't have access to this command!");
+                        }
+                        return false;
+                    }
+                    return sourcePlayer.PlayerId == PlayerControl.LocalPlayer.PlayerId;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(ChatBubble), nameof(ChatBubble.SetName))]
+        public class ChatFixName
+        {
+            public static bool Prefix(ChatBubble __instance)
+            {
+                if (error)
+                {
+                    __instance.NameText.text = "ERROR";
+                    __instance.NameText.color = Palette.ImpostorRed;
+                    __instance.NameText.ForceMeshUpdate(true, true);
+                    __instance.Xmark.enabled = true;
+                    __instance.votedMark.enabled = false;
+                    error = false;
+                    return false;
+                }
+                else if (system)
+                {
+                    __instance.NameText.text = "SYSTEM MESSAGE";
+                    __instance.NameText.color = Palette.CrewmateBlue;
+                    __instance.NameText.ForceMeshUpdate(true, true);
+                    __instance.Xmark.enabled = false;
+                    __instance.votedMark.enabled = false;
+                    system = false;
+                    return false;
+                }
+                else if (noaccess)
+                {
+                    __instance.NameText.text = "NO ACCESS";
+                    __instance.NameText.color = Palette.Blue;
+                    __instance.NameText.ForceMeshUpdate(true, true);
+                    __instance.Xmark.enabled = true;
+                    __instance.votedMark.enabled = false;
+                    noaccess = false;
+                    return false;
+                }
+                else return true;
+            }
+        }
+    }
+}
